481e662c55bf0a694b02a56f84e12a846b1b08aa
Ahmedmustaffa <89359266+Ahmedmustaffa@users.noreply.github.com>
2026-05-15 15:31:17 +0300

    Add email field to User schema and format code

--- server/models/User.js
+++ server/models/User.js
@@ -2,20 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
-    userName: { type: String, required: true, unique: true , minLength : 3, maxLength : 20},
-    password: { type: String, required: true , minLength : 8, maxLength : 25 , trim : true , select : false},
+    userName: { type: String, required: true, unique: true, minLength: 3, maxLength: 20 },
+    password: { type: String, required: true, minLength: 8, maxLength: 25, trim: true, select: false },
+    email: { type: String, required: true, unique: true },
     favorite: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
-    
+
 }, { timestamps: true });
 
-userSchema.pre('save', async function() {
-    if (!this.isModified('password')) return ;
-
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return;
     this.password = await bcrypt.hash(this.password, 12);
-    ;
 });
 
-userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
     return await bcrypt.compare(candidatePassword, userPassword);
 };
 
